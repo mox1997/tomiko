c13a987789e5b88eda64fae94c9e97c68dcb238a
Grigoriev Maxim <mg8219002@gmail.com>
2024-12-27 02:52:56 +1000

    Initial commit

---
+++ src/apps/header/context_processors.py
@@ -0,0 +1,8 @@
+def header_context(request):
+    return {
+        'phone_number': '+7 (000) 000-00-00',
+        'social_links': {
+            'telegram': 'https://telegram.me/example',
+            'whatsapp': 'https://wa.me/00000000000',
+        },
+    }
